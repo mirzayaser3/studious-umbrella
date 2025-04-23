0cd2221f0ae4cf1545e19989e026cd657c63c443
Abstract programmer <Abstractprogrammer@example.com>
2025-04-23 15:46:32 +0530

    simple_program.py

---
+++ Other/maviya/simple_program.py
@@ -0,0 +1,11 @@
+# Simple Pyramid Program
+def simple_pyramid(height):
+    for i in range(1, height + 1):
+        print("*" * i)
+
+# Get the height of the pyramid from the user
+try:
+    height = int(input("Enter the height of the pyramid: "))
+    simple_pyramid(height)
+except ValueError:
+    print("Please enter a valid integer.")
